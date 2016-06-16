ede3efcaac1271779df5280d60e87e2fdf753385
Yijin Kang <yijinkang@gmail.com>
2016-06-16 14:36:41 -0700

    switches between table and collection views. collection view could look better...

---
+++ Flix/Flix/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  Flix
+//
+//  Created by Yijin Kang on 6/16/16.
+//  Copyright © 2016 Yijin Kang. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
